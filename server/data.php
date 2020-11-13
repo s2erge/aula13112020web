802c596b8bf746d801cf2c6355db3492d43af7fc
s2erge <sergemaia9@gmail.com>
2020-11-13 10:55:32 +0000

    CSS Version

---
+++ server/data.php
@@ -0,0 +1,82 @@
+<?php 
+
+    $students = [
+        [
+            "code" => "190100249",
+            "name" => "Alex Ferreira Pires",
+        ],
+        [
+            "code" => "190100210",
+            "name" => "Alexandre José Costa Figueiredo",
+        ],
+        [
+            "code" => "190100216",
+            "name" => "Alexandre Miguel do Couto da Silva",
+        ],
+        [
+            "code" => "190100262",
+            "name" => "Daniel Marques Vicente",
+        ],
+        [
+            "code" => "190100282",
+            "name" => "Duarte Filipe Arriaga Santos",
+        ],
+        [
+            "code" => "190100225",
+            "name" => "Francisco José Pina da Silva Esteves",
+        ],
+        [
+            "code" => "190100488",
+            "name" => "Gonçalo Manuel Costa dos Santos",
+        ],
+        [
+            "code" => "190100209",
+            "name" => "Hugo Filipe da Conceição Figueiredo",
+        ],
+        [
+            "code" => "190100237",
+            "name" => "João Carlos Casaca Vinagre",
+        ],
+        [
+            "code" => "190100218",
+            "name" => "João Manuel Coelho Teixeira",
+        ],
+        [
+            "code" => "190100217",
+            "name" => "João Miguel Fernandes Ameixa",
+        ],
+        [
+            "code" => "190100263",
+            "name" => "Manuel Bernardo Nunes de Oliveira",
+        ],
+        [
+            "code" => "190100208",
+            "name" => "Márcio Alexandre Cintra Vilarinho Henriques",
+        ],
+        [
+            "code" => "190100269",
+            "name" => "Miguel Alexandre da Silva Serdeira",
+        ],
+        [
+            "code" => "190100260",
+            "name" => "Miguel Énio Gomes da Silva",
+        ],
+        [
+            "code" => "180100341",
+            "name" => "Nuno Miguel Mendes da Fonseca de Cardoso Assembleia",
+        ],
+        [
+            "code" => "190100315",
+            "name" => "Ricardo Filipe da Silva Ramos Duarte",
+        ],
+        [
+            "code" => "190100331",
+            "name" => "Serge Maia da Silva",
+        ],
+        [
+            "code" => "190100306",
+            "name" => "Weberth Rosa de Souza",
+        ]
+    ];
+
+?>
